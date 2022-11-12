c59c9aca6329ae1d34455316377e878f9ef7ab7a
Amir Esmaeili <amir79esmaeili@gmail.com>
2022-11-13 02:36:52 +0330

    M: only get available coupons on view

--- apps/coupons/views.py
+++ apps/coupons/views.py
@@ -1,13 +1,18 @@
+from datetime import datetime
+
 from django.db.models import F
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
 
 from .models import Coupon
-
 from .serializers import CouponsSerializer
 
 
 class CouponsList(ListAPIView):
-    queryset = Coupon.objects.filter(used_by__lt=F("capacity"))
+    queryset = (
+        Coupon.objects.filter(used_by__lt=F("capacity"))
+        .filter(is_active=True)
+        .filter(expire_at__lte=datetime.now())
+    )  # only get available coupons
     serializer_class = CouponsSerializer
     permission_classes = (AllowAny,)
