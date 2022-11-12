6a39d8fef9859f93f20e8b0f2535df7aae8ed302
Amir Esmaeili <amir79esmaeili@gmail.com>
2022-11-13 02:02:47 +0330

    + coupon admin panel

--- apps/coupons/admin.py
+++ apps/coupons/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Coupon
 
-# Register your models here.
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ("code", "balance", "capacity", "expire_at")
+    search_fields = ("code",)
+    list_filter = ("expire_at",)
