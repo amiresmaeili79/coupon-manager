c94133dbe339ace3a341dcec603620472e4475e0
Amir Esmaeili <amir79esmaeili@gmail.com>
2022-11-12 22:24:01 +0330

    base project template

---
+++ config/local.py
@@ -0,0 +1,20 @@
+import os
+
+from .base import *
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+DEBUG = True
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+    }
+}
