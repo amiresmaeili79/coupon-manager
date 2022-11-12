0e4eeed181c107d1b01610014966cf381cbb3bbb
Amir Esmaeili <amir79esmaeili@gmail.com>
2022-11-13 00:18:43 +0330

    + JWT token support & login view

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -2,10 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 from apps.shared.models import FeaturedBaseModel
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class Account(AbstractUser, FeaturedBaseModel):
     balance = models.PositiveBigIntegerField(default=0)
+
+    @property
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {"access": str(refresh.access_token), "refresh": str(refresh)}
 
     class Meta:
         db_table = "accounts"
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Account
+from .serializers import LoginSerializer
+
+
+class LoginView(APIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LoginSerializer
+
+    def post(self, request: "Request") -> "Response":
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user: Account = authenticate(
+            username=serializer.data.get("username"),
+            password=serializer.data.get("password"),
+        )
+        if (user is None) or (not user.is_active):
+            return Response(
+                "Username or password is not correct",
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        return Response(user.tokens, status=status.HTTP_200_OK)
