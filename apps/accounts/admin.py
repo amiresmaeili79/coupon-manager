d57be860315001adeaec8c0d18aa067f5388d4f8
Amir Esmaeili <amir79esmaeili@gmail.com>
2022-11-12 23:08:03 +0330

    created admin panel for accounts

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Account
+
+
+@admin.register(Account)
+class AccountsAdmin(admin.ModelAdmin):
+    list_display = ("username", "balance", "date_joined")
+    search_fields = ("username", "id")
+    list_filter = ("is_superuser", "is_staff")
+
+    def get_readonly_fields(self, request, obj=None):
+        return [f.name for f in obj._meta.fields if not f.editable]
