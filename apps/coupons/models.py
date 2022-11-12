41d297c24e44c20bd352947828ecc7dd0c91aa30
Amir Esmaeili <amir79esmaeili@gmail.com>
2022-11-13 01:57:46 +0330

    + Coupon model

--- apps/coupons/models.py
+++ apps/coupons/models.py
@@ -1,3 +1,19 @@
+from apps.shared.models import FeaturedBaseModel
 from django.db import models
 
-# Create your models here.
+
+class Coupon(FeaturedBaseModel):
+    code = models.CharField(max_length=50, db_index=True)
+    balance = models.PositiveBigIntegerField(default=0)
+    capacity = models.PositiveIntegerField()
+    expire_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.code
+
+    def __repr__(self):
+        return f"Coupon({self.code}, {self.balance}, {self.capacity}, {self.expire_at})"
+
+    class Meta:
+        db_table = "coupons"
+        ordering = ("expire_at",)
